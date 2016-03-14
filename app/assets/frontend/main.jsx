9a1ac29c2a4ad1a53d1952682d8e361934c0f434
mauricioedu <olive.y.ra@hotmail.com>
2016-03-14 09:45:16 -0300

    gravando tweets

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -15,9 +15,20 @@ class Main extends React.Component{
   }
 
   addTweet(tweetToAdd) {
-    let newTweetsList = this.state.tweetsList;
-    newTweetsList.unshift({ id: Date.now(), name: 'Gest', body: tweetToAdd })
-    this.setState({ tweetsList: newTweetsList })
+    $.post("/tweets", { body: tweetToAdd })
+    .success( savedTweet => {
+      let newTweetsList = this.state.tweetsList;
+      newTweetsList.unshift( savedTweet );
+      this.setState({ tweetsList: newTweetsList });
+    })
+    .error(error => console.log(error));
+
+
+  }
+  componentDidMount() {
+    $.ajax("/tweets")
+    .success(data => this.setState({ tweetsList: data }))
+    .error(error => console.log(error));
   }
 
   render() {
